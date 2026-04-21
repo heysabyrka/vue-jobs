bf959315218a95cdd6863d5bd7a5c189b14dedc8
Sabyrka <heysabyrka@gmail.com>
2026-04-21 20:05:13 +0500

    feat: add AppHero component for enhanced user engagement

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
 import AppNavbar from './components/AppNavbar.vue'
+import AppHero from './components/AppHero.vue'
 </script>
 
 <template>
   <AppNavbar />
+  <AppHero />
 </template>
